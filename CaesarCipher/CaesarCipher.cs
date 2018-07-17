4557ea2dde786a2569c5d07d6073a7a501f0a8ce
Srikanth Banagadi <amisrikanth@gmail.com>
2018-07-17 18:21:39 +0530

    I have to optimise this

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -7,7 +7,26 @@ namespace CaesarCipher
     {
         public static string Rotate(string text, int shiftKey)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            char[] textArray = text.ToCharArray();
+            for(int i=0;i<textArray.Length;i++)
+            {
+                if (Convert.ToInt32(textArray[i]) >= 65 && Convert.ToInt32(textArray[i]) <= 90)
+                {
+                    if(Convert.ToInt32(textArray[i]) + shiftKey > 90)
+                        textArray[i] = (char)(Convert.ToInt32(textArray[i]) + shiftKey - 26);
+                    else
+                    textArray[i] = (char)(Convert.ToInt32(textArray[i]) + shiftKey);
+                }
+                else if(Convert.ToInt32(textArray[i]) >= 97 && Convert.ToInt32(textArray[i]) <= 122)
+                {
+                    if (Convert.ToInt32(textArray[i]) + shiftKey > 122)
+                        textArray[i] = (char)(Convert.ToInt32(textArray[i]) + shiftKey - 26);
+                    else
+                        textArray[i] = (char)(Convert.ToInt32(textArray[i]) + shiftKey);
+                }
+            }
+            String cipherText = new String(textArray);
+            return cipherText;
         }
     }
 }
